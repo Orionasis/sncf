6e2af1ad02a21112885cee42b4e638fd0d24616b
Orionasis <100146226+Orionasis@users.noreply.github.com>
2022-03-17 23:33:13 +0100

    Add files via upload

--- main (8).cpp
+++ main (8).cpp
@@ -1,26 +1,45 @@
-#include <fstream>
-#include <sstream>
-#include <string.h>
 #include <iostream>
 #include <vector>
-#include "json.h"
-#include <iterator>
-#include "wait.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <dirent.h>
-#include <algorithm>
-#include <unistd.h>
+#include <sys/types.h>
 #include <stdlib.h>
 
-using namespace std;
-
-int main(int argc, char*argv[])
+int main(int argc, char* argv[])
 {
-    JSON json;
-    std::string name=argv[2];
-    std::vector<std::vector<std::string>> x_js;
-    json.read_json(name, x_js);
-    json.creationsjson(argv[1],argv[2],x_js);
+    std::ofstream flux_sortie(argv[1],std::ios::out|std::ios::trunc);
+    //test si le fichier de sortie 
+        if(flux_sortie.fail()){
+            std::cerr<<"le fichier n'existe pas "<<argv[1]<<std::endl;
+            return 1;
+        }
+        flux_sortie.precision(12);
+        std::string bodysources="ARGS=/home/devops/line\n"
+                "EXEC=../build-line3-Desktop-Du00e9faut/line3\n"
+
+                "all: exec\n"
+
+                "exec: $(EXEC)\n"
+                    "\t./$< $(ARGS)\n";
+                    //écrit dans le flux de sortie la chaîne de caractère
+        flux_sortie<<bodysources;
+        std::string sortie=argv[1];
+        int nbthreads = 5;
+            std::string s;
+            std::stringstream out;
+            out << nbthreads;
+            s = out.str();
+            //fait une parallélisation mixte
+std::string make= "make -f"+sortie+" -j "+s;
+if(0!=system(make.c_str())){
+        std::cerr<<"le fichier n'existe pas "<<sortie<<std::endl;
+        return 1;
+    }
     return 0;
+
+
 }
